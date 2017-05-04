ee85b185d06897548f2f6449b78955bf1f9a776b
mattw46 <matthew_w46@yahoo.com.au>
2017-05-04 11:59:23 +0000

    added first functions

--- js-lib-common.js
+++ js-lib-common.js
@@ -2,3 +2,19 @@
  My personal JS library
  Commonly used functions to be imported to applications
 */
+
+// Reverse a String
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// factorialize a Number
+function factorialize(num) {
+  var factorial = 1;
+  var counter = 2;
+  while (counter <= num) {
+    factorial *= counter;
+    counter++;
+  }
+  return factorial;
+}
